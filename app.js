5036c7bb5294f8c7160819260410dc41f5f8270d
Magatijoel9620 <joeldamarko@gmail.com>
2025-07-21 14:37:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -70,3 +70,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+auth: {
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS,
+},
